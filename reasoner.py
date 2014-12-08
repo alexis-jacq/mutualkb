9adf08c0d69c32c44b9faf6e6ca174bb407c45bd
Alexis Jacq <alexis.jacq@epfl.ch>
2014-12-08 17:22:43 +0100

    added agents in db + improve mutual-reasoner

--- reasoner.py
+++ reasoner.py
@@ -6,6 +6,8 @@ from kb import TABLENAME, KBNAME
 
 REASONER_RATE = 5 #Hz
 
+# add M operator et K operator
+
 class OntoClass():
 
     def __init__(self, name):
@@ -90,10 +92,16 @@ class reasoner():
         
         def addinstance(instance, cls):
             newrdftype.add((instance, cls.name))
-            cls.instances.add(instance)
-            for p in frozenset(cls.parents):
+            for p in cls.parents:
                 addinstance(instance, p)
 
+        def addoverclassof(cls, ocls):
+            newsubclassof.add((cls.name, ocls.name))
+            ocls.children.add(cls)
+            cls.parents.add(ocls)
+            for c in frozenset(cls.children):
+                addoverclassof(c, cls)
+                
         def addsubclassof(scls, cls):
             newsubclassof.add((scls.name, cls.name))
             cls.children.add(scls)
@@ -110,20 +118,22 @@ class reasoner():
                 for e in frozenset(equ.equivalents):
                     addequivalent(cls, e, memory)
 
-        for name, cls in onto.items():           # for name, cls in onto_active.items()
-            for i in frozenset(cls.instances): 
-                addinstance(i, cls)
+        for name, cls in onto.items():        # just activated onto.items() could be interesting
             for p in frozenset(cls.parents):
                 addsubclassof(cls, p)
+            for i in cls.instances: 
+                addinstance(i, cls)
             
             memory = set()
             for equivalent in frozenset(cls.equivalents):
                 addequivalent(cls, equivalent, memory)
             for equivalent in cls.equivalents:
-                for i in frozenset(cls.instances):
-                    addinstance(i, equivalent)
                 for p in frozenset(cls.parents):
                     addsubclassof(equivalent, p)
+                for c in frozenset(cls.children):
+                    addoverclassof(c, equivalent)
+                for i in cls.instances:
+                    addinstance(i, equivalent)
 
                 
         
@@ -171,8 +181,111 @@ class reasoner():
     # MUTUAL MODELING methods
     #---------------------------
 
-    # ...
+    def get_mutual_knowledge(self, db):
 
+        visualknowledge = None
+        with db:
+            visualknowledges = {(row[0], row[1], row[2]) for row in db.execute(
+                    '''SELECT subject, object, model FROM %s WHERE subject!='self' AND subject in 
+                    (SELECT subject FROM %s WHERE (predicate='rdf:type' AND  object='agent')) 
+                    AND object in 
+                    (SELECT subject FROM %s WHERE (predicate='rdf:type' AND  object='agent'))
+                    AND predicate="canSee" 
+                    ''' % (TABLENAME, TABLENAME, TABLENAME))}
+            selfknowledges = {(row[0], row[1]) for row in db.execute(
+                    '''SELECT subject, model FROM %s WHERE subject!='self' AND subject in 
+                    (SELECT subject FROM %s WHERE (predicate='rdf:type' AND  object='agent')) 
+                    ''' % (TABLENAME, TABLENAME))}
+        
+        return visualknowledges, selfknowledges
+
+
+    def update_models(self):
+        
+        ok = self.copydb()
+        if not ok:
+            return
+        
+        newstmts = []
+            
+        visualknowledges, selfknowledge = self.get_mutual_knowledge(self.db)
+        
+        trans_know = {}
+        
+        for agent, model in selfknowledge:
+            
+            # the agents are self-conscious :
+            #--------------------------------
+            spl_model = model.replace('_',' _ ').replace(':',' : ').split()
+            
+            new_spl_model = [''.join(spl_model[:-3])] + ['M_'] + [''.join(spl_model[-1])] + [':K_'] + [agent]
+            new_model = ''.join(new_spl_model)
+            
+            generalknowledge = {(row[0], row[1]) for row in self.db.execute(
+                        '''SELECT predicate, object FROM %s WHERE subject="%s"
+                        AND model="%s" AND topic="general" ''' % (TABLENAME, agent, model))}
+            
+            newstmts += [('self', 'rdf:type', 'agent', new_model)]
+            new_model_mk = trans_know.setdefault((new_model,agent),set())
+            new_model_mk.add(('rdf:type','agent'))
+            model_mk = trans_know.setdefault((model,agent),set())
+            model_mk.add(('rdf:type','agent'))
+            
+            if generalknowledge:
+                for p,o in generalknowledge:
+                    newstmts += [('self', p, o, new_model)]
+                    new_model_mk = trans_know.setdefault((new_model,agent),set())
+                    new_model_mk.add((p,o))
+                    model_mk = trans_know.setdefault((model,agent),set())
+                    model_mk.add((p,o))
+        
+        for agent1, agent2, model in visualknowledges:
+            
+            # take care of the concerned agent2 :
+            if agent2=='self':
+                agent2 = model.replace('_',' ').split()[-1]
+            if agent2==agent1:
+                agent2 = 'self'
+            
+            # agent1 knows agent2 is an agent :
+            #----------------------------------
+            new_spl_model2 = [''.join(spl_model[:-3])] + ['M_'] + [''.join(spl_model[-1])] + [':K_'] + [agent1]
+            new_model2 = ''.join(new_spl_model2)
+            
+            newstmts += [(agent2, 'rdf:type', 'agent', new_model2)]
+            new_model2_mk = trans_know.setdefault((new_model2,agent2),set())
+            new_model2_mk.add(('rdf:type','agent'))
+            
+            k_from_model = trans_know.setdefault((model,agent2),set())
+            if k_from_model:
+                for p,o in k_from_model:
+                    newstmts += [(agent2, p, o, new_model2)]
+                    new_model2_mk = trans_know.setdefault((new_model2,agent2),set())
+                    new_model2_mk.add((p,o))
+            
+            # agent1 knows agent2 is self-conscious :
+            #-----------------------------------------
+            spl_model = model.replace('_',' _ ').replace(':',' : ').split()
+            
+            new_spl_model1 = [''.join(spl_model[:-3])] + ['M_'] + [''.join(spl_model[-1])] + [':M_'] + [agent1] + [':K_'] + [agent2]
+            new_model1 = ''.join(new_spl_model1)
+            
+                        
+            newstmts += [('self', 'rdf:type', 'agent', new_model1)]
+            new_model1_mk = trans_know.setdefault((new_model1,agent2),set())
+            new_model1_mk.add(('rdf:type','agent'))
+            
+            k_from_model = trans_know.setdefault((new_model2,agent2),set())
+            if k_from_model:
+                for p,o in k_from_model:
+                    newstmts += [('self', p, o, new_model1)]
+                    new_model1_mk = trans_know.setdefault((new_model1,agent2),set())
+                    new_model1_mk.add((p,o))
+                    
+        
+        if newstmts:
+            
+            self.update_shared_db(newstmts,'general')
 
 
     # UPDATE methods
@@ -185,7 +298,7 @@ class reasoner():
             with self.db:
                 self.db.execute("DELETE FROM %s" % TABLENAME)
                 self.db.executemany('''INSERT INTO %s
-                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''' % TABLENAME,
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''' % TABLENAME,
                     res)
             return True
         except sqlite3.OperationalError:
@@ -196,12 +309,12 @@ class reasoner():
 
     def update_shared_db(self, stmts, topic):
         
-        nodes = [[ s, p, o, model, "%s%s%s%s"%(s,p,o, model), topic ] for s,p,o,model in stmts]
+        nodes = [[ s, p, o, model, model.replace('_',' ').split()[-1], "%s%s%s%s"%(s,p,o, model), topic ] for s,p,o,model in stmts]
 
         with self.shareddb:
             self.shareddb.executemany('''INSERT OR IGNORE INTO %s
-                 (subject, predicate, object, model, id, topic)
-                 VALUES (?, ?, ?, ?, ?, ?)''' % TABLENAME, nodes) 
+                 (subject, predicate, object, model, agent, id, topic)
+                 VALUES (?, ?, ?, ?, ?, ?, ?)''' % TABLENAME, nodes) 
             self.shareddb.commit()
 
 def reasoner_start():
@@ -218,6 +331,7 @@ if __name__=='__main__':
 
     reason = reasoner()
     reason.classify()
+    reason.update_models()
     reason.shareddb.close()
     reason.db.close()
 
